010df10b1436d8b199f7bca7168854edd1b58291
Eric Vrenios <eric.vrenios@flextronics.com>
2015-09-28 17:13:58 -0500

    added directorycollection

---
+++ Material/Controls/FileManager/DirectoryCollection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+
+namespace Material.Controls.FileManager
+{
+//	[Serializable]
+	//public class DirectoryCollection : Collection<DirectoryBinder>
+	//{
+	//	public DirectoryCollection() { }
+
+	//	public DirectoryCollection(IEnumerable<string> filePaths)
+	//	{
+	//		foreach (var i in filePaths)
+	//		{
+	//			Add(new DirectoryBinder(new DirectoryInfo(i)));
+	//		}
+	//	}
+	//}
+}
